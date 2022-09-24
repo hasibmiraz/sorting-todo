8173901e34a542fdd4f995cbf895924c34f0c99a
hasibmiraz <hasibmiraz09@gmail.com>
2022-09-24 15:47:06 +0600

    Added add form

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -5,7 +5,7 @@ import { MdOutlineCancel } from 'react-icons/md';
 const Tasks = () => {
   return (
     <section className="space-y-4">
-      <div className="h-12 w-96 bg-blue-100 flex justify-between items-center px-3 rounded-lg">
+      <div className="h-14 w-96 bg-blue-200 flex justify-between items-center px-3 rounded-lg">
         <p className="font-semibold text-gray-600 text-lg">Todo</p>
         <div className="flex space-x-2">
           <GoTriangleUp
@@ -24,6 +24,20 @@ const Tasks = () => {
             size={20}
           />
         </div>
+      </div>
+      <div className="h-14 w-96 bg-blue-200 flex justify-between items-center px-3 rounded-lg">
+        <form className="w-full flex justify-between">
+          <input
+            type="text"
+            className="w-[80%] bg-blue-100 focus:outline-blue-600 h-8 rounded px-2"
+          />
+          <input
+            type="submit"
+            value="Add"
+            className="text-center bg-blue-600 text-white w-[18%] rounded cursor-pointer hover:scale-95 duration-150"
+          />
+        </form>
+        <div className="flex space-x-2"></div>
       </div>
     </section>
   );
