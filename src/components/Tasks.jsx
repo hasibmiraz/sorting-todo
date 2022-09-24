c4bf33ba7e950f0cb507ea5faad846e6e6898ca0
hasibmiraz <hasibmiraz09@gmail.com>
2022-09-24 14:37:38 +0600

    Static ui complete

---
+++ src/components/Tasks.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { GoTriangleUp, GoTriangleDown } from 'react-icons/go';
+import { MdOutlineCancel } from 'react-icons/md';
+
+const Tasks = () => {
+  return (
+    <section className="space-y-4">
+      <div className="h-12 w-96 bg-blue-100 flex justify-between items-center px-3 rounded-lg">
+        <p className="font-semibold text-gray-600 text-lg">Todo</p>
+        <div className="flex space-x-2">
+          <GoTriangleUp className="text-blue-600 cursor-pointer" size={20} />
+          <GoTriangleDown className="text-blue-600 cursor-pointer" size={20} />
+          <MdOutlineCancel className="text-red-500 cursor-pointer" size={20} />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Tasks;
