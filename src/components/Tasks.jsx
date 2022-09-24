28a12e2486e6ae4fd01eff8bb0c0b6c6df428cac
hasibmiraz <hasibmiraz09@gmail.com>
2022-09-24 15:04:28 +0600

    firebase setup complete

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -8,9 +8,21 @@ const Tasks = () => {
       <div className="h-12 w-96 bg-blue-100 flex justify-between items-center px-3 rounded-lg">
         <p className="font-semibold text-gray-600 text-lg">Todo</p>
         <div className="flex space-x-2">
-          <GoTriangleUp className="text-blue-600 cursor-pointer" size={20} />
-          <GoTriangleDown className="text-blue-600 cursor-pointer" size={20} />
-          <MdOutlineCancel className="text-red-500 cursor-pointer" size={20} />
+          <GoTriangleUp
+            className="text-blue-600 cursor-pointer"
+            title="Move Up"
+            size={20}
+          />
+          <GoTriangleDown
+            className="text-blue-600 cursor-pointer"
+            title="Move Down"
+            size={20}
+          />
+          <MdOutlineCancel
+            className="text-red-500 cursor-pointer"
+            title="Delete"
+            size={20}
+          />
         </div>
       </div>
     </section>
